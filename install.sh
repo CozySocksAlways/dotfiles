423935b41c35644fc8a3bde149a9e236681826cf
Harris Antony Jos <ha.jos@fz-juelich.de>
2025-12-17 11:43:18 +0100

    install and gitconfig files

---
+++ install.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+# exit immediately if any command fails
+set -e
+
+DOTFILES="$HOME/dotfiles"
+
+ln -sf "$DOTFILES/.vimrc" "$HOME/.vimrc"
+ln -sf "$DOTFILES/.inputrc" "$HOME/.inputrc"
