b38eb6243ca1372f234119e0017756c275c9b125
Harris Antony Jos <ha.jos@fz-juelich.de>
2026-02-01 07:24:30 +0100

    vscode theme modified and diffview added

--- plugin_nvim_config/lua/plugins/vscodetheme.lua
+++ plugin_nvim_config/lua/plugins/vscodetheme.lua
@@ -1,5 +1,14 @@
 return {
 	{
-		'Mofiqul/vscode.nvim'
+		'Mofiqul/vscode.nvim',
+		config = function()
+			local c = require('vscode.colors').get_colors()
+			require("vscode").setup {
+				italic_comments = true,
+				group_overrides = {
+					Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
+				}
+			}
+		end
 	}
 }
